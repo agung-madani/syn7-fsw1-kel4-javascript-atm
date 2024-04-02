34e5d0074d9894f50127464f7c139189b40e03e8
XisUndefined <muhlisahmadabdillah_1306619028@mhs.unj.ac.id>
2024-04-03 06:22:55 +0700

    feat: add validateCardNumber function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const readLine = require('readline');
+const readLine = require("readline");
 
 const rl = readLine.createInterface({
   input: process.stdin,
@@ -6,24 +6,45 @@ const rl = readLine.createInterface({
 });
 
 const accountA = {
-  name: 'John Doe',
+  name: "John Doe",
   balance: 0,
-  pin: '123456',
-  cardNumber: '1234 5678 1234 5678',
+  pin: "123456",
+  cardNumber: "1234 5678 1234 5678",
   transactions: [],
 };
 
 const accountB = {
-  name: 'Jane Doe',
+  name: "Jane Doe",
   balance: 0,
-  pin: '654321',
-  cardNumber: '5678 1234 5678 1234',
+  pin: "654321",
+  cardNumber: "5678 1234 5678 1234",
   transactions: [],
 };
 
 const accounts = [accountA, accountB];
 
-function validateCardNumber() {}
+function validateCardNumber(accountArray) {
+  return accountArray.map((account) => {
+    const numericCardNumber = account.cardNumber.replace(/\D/g, "");
+
+    if (numericCardNumber.length !== 16) {
+      return false;
+    }
+
+    let sum = 0;
+    for (let i = numericCardNumber.length - 1; i >= 0; i--) {
+      let digit = parseInt(numericCardNumber[i], 10);
+      if ((numericCardNumber.length - i) % 2 === 0) {
+        digit *= 2;
+        if (digit > 9) {
+          digit -= 9;
+        }
+      }
+      sum += digit;
+    }
+    return sum % 10 === 0;
+  });
+}
 function validatePin() {}
 function checkBalance() {}
 function deposit() {}
@@ -39,13 +60,13 @@ function askQuestion(question) {
 
 async function main() {
   do {
-    console.log('Menu ATM:');
-    console.log('1. Cek Saldo');
-    console.log('2. Setor Tunai');
-    console.log('3. Riwayat Transaksi');
-    console.log('4. Keluar');
+    console.log("Menu ATM:");
+    console.log("1. Cek Saldo");
+    console.log("2. Setor Tunai");
+    console.log("3. Riwayat Transaksi");
+    console.log("4. Keluar");
 
-    choice = await askQuestion('Masukkan pilihan Anda: ');
+    choice = await askQuestion("Masukkan pilihan Anda: ");
 
     switch (parseInt(choice)) {
       case 1:
